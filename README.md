dce140da747cafe2a83f443fa339d0b6a3631874
Lei Xu <komushi@gmail.com>
2018-10-27 09:55:36 +0900

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-cli-plugin-amplify
+
+This is a vue-cli 3.x plugin which adds AWS Amplify to your Vue project.
---
+++ generator.js
@@ -0,0 +1,25 @@
+module.exports = (api, options, rootOptions) => {
+  // modify package.json fields
+  api.extendPackage({
+    pkg: {
+      dependencies: {
+        'aws-amplify': 'latest',
+        'aws-appsync': 'latest',
+        'graphql': 'latest',
+        'vue-apollo': 'latest',
+        'vue-router': 'latest',
+      },
+      devDependencies: {
+        'graphql-tag': '^2.9.0',
+        'eslint-plugin-graphql': '^2.1.0',
+      },
+    }
+  })
+
+  // copy and render all files in ./template with ejs
+  api.render('./template')
+
+  if (options.foo) {
+    // conditionally generate files
+  }
+}
